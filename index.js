c912e2b07ae6cb061264dc6b8341a6a70c773f7a
Aaron Hong <hong.aaron.c@gmail.com>
2015-10-24 00:23:45 -0700

    Compatibility fixes with IE

--- index.js
+++ index.js
@@ -29,15 +29,18 @@ function activateSelected() {
         cards[i].style.left = borderSpaceLeft + "px";
         cards[i].style.opacity = "1";
         cards[i].style.webkitFilter = "grayscale(90%) brightness(50%)";
+        cards[i].style.filter = "grayscale(90%) brightness(50%)";
     }
     for (var i = selectedIndex + 1; i < cards.length; i++) {
         cards[i].style.left = document.getElementById("clipping_container").clientWidth - ((cards.length - 1) * cardSpacing) - borderSpaceRight + "px";
         cards[i].style.opacity = "1";
         cards[i].style.webkitFilter = "grayscale(90%) brightness(50%)";
+        cards[i].style.filter = "grayscale(90%) brightness(50%)";
     }
     //selectedCard.style.left = ((window.innerWidth - selectedCard.offsetWidth) / 2) + "px";    Use this if you want the selected card to be centered
     selectedCard.style.left = borderSpaceLeft + "px";   //Use this if you want the select card to be left aligned
     selectedCard.style.opacity = "1.0";
     selectedCard.style.webkitFilter = "grayscale(0%) brightness(100%)";
+    selectedCard.style.filter = "grayscale(0%) brightness(100%)";
     document.body.style.backgroundColor = selectedCard.getElementsByClassName("card_header")[0].style.backgroundColor;
 }
