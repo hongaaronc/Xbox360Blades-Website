e52ff032052ad63ac84abc5438ff9d8f7c5a1923
Aaron Hong <aaronhon@usc.edu>
2016-01-13 19:46:19 -0800

    Added content for personal website

--- index.js
+++ index.js
@@ -25,18 +25,19 @@ function activateSelected() {
             selectedIndex = i;
         }
     }
-    for (var i = 0; i < selectedIndex; i++) {
+    for (var i = 0; i < selectedIndex; i++) {   //For all cards to the left of the selected card
         cards[i].style.left = borderSpaceLeft + "px";
         cards[i].style.opacity = "1";
         cards[i].style.webkitFilter = "grayscale(90%) brightness(50%)";
         cards[i].style.filter = "grayscale(90%) brightness(50%)";
     }
-    for (var i = selectedIndex + 1; i < cards.length; i++) {
+    for (var i = selectedIndex + 1; i < cards.length; i++) {    //For all cards to the right of the selected card
         cards[i].style.left = document.getElementById("clipping_container").clientWidth - ((cards.length - 1) * cardSpacing) - borderSpaceRight + "px";
         cards[i].style.opacity = "1";
         cards[i].style.webkitFilter = "grayscale(90%) brightness(50%)";
         cards[i].style.filter = "grayscale(90%) brightness(50%)";
     }
+    //For the selected card
     //selectedCard.style.left = ((window.innerWidth - selectedCard.offsetWidth) / 2) + "px";    Use this if you want the selected card to be centered
     selectedCard.style.left = borderSpaceLeft + "px";   //Use this if you want the select card to be left aligned
     selectedCard.style.opacity = "1.0";
